fa1b36bcb869b6571661d658cb62516f815a95d0
Trần Quốc Thuận <quocthuan251@gmail.com>
2021-03-16 13:08:47 +0700

    feat: Add feature Signin

---
+++ src/features/SigninFeature/components/SigninBackgroud/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './SigninBackgroud.scss'
+function SigninBackgroud() {
+	return (
+        <div id="particle-container">
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+        <div className="particle"></div>
+    </div>
+        
+	);
+}
+
+export default SigninBackgroud;
---
+++ src/features/SigninFeature/index.jsx
@@ -0,0 +1,95 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Card, Checkbox, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
+import "./SigninFeature.scss";
+import SigninBackgroud from './components/SigninBackgroud'
+import logo from '../../assets/images/logo.svg'
+	export default function SigninFeature() {
+		const onFinish = (values) => {
+			console.log('Received values of form: ', values);
+		};
+	
+		return (
+      <div >
+    <SigninBackgroud></SigninBackgroud>
+
+  		<div className="signin-feature-container" >
+        <div>
+          <img src= {logo} alt="" className="signin-feature-header-logo"/>
+          <div className="signin-feature-header-title">Trang quản lý Chill music</div>
+        </div>
+      
+			<Card title="Đăng nhập" bordered={false} className="signin-feature-card-box" style={{backgroundColor: "#000D0D", color: '#FFFFFF'}}>
+        <Form
+					name="normal_login"
+					className="login-form"
+          
+					initialValues={{
+						remember: true,
+					}}
+					onFinish={onFinish}
+          size= "large"
+				>
+					<Form.Item
+						name="username"
+						rules={[
+							{
+								required: true,
+								message: 'Please input your Username!',
+							},
+						]}
+					>
+						<Input
+							prefix={
+								<UserOutlined className="site-form-item-icon" />
+							}
+							placeholder="Username"
+						/>
+					</Form.Item>
+					<Form.Item
+						name="password"
+						rules={[
+							{
+								required: true,
+								message: 'Please input your Password!',
+							},
+						]}
+					>
+						<Input
+							prefix={
+								<LockOutlined className="site-form-item-icon" />
+							}
+							type="password"
+							placeholder="Password"
+						/>
+					</Form.Item>
+					<Form.Item>
+						<Form.Item
+							name="remember"
+							valuePropName="checked"
+							noStyle
+						>
+							<Checkbox >Nhớ mật khẩu</Checkbox>
+						</Form.Item>
+
+					
+					
+					</Form.Item>
+
+					<Form.Item>
+						<Button
+							type="primary"
+							htmlType="submit"
+							className="signin-feature-form-button-submit"
+						>
+							Đăng nhập
+						</Button>
+						
+            
+					</Form.Item>
+				</Form>
+			</Card>
+		</div>
+	</div>
+		);
+	};
