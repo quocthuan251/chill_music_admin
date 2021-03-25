0b2d046ea8584be7f235cfcb524b39bc907f9823
Trần Quốc Thuận <quocthuan251@gmail.com>
2021-03-25 08:39:53 +0700

    feat: add react redux saga

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import 'antd/dist/antd.css'
-
+import { Provider } from 'react-redux'
+import configureStore from './store'
+const initialState = {}
+const { store } = configureStore(initialState)
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 )
